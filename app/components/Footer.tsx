babcb967b81a0a4a6a323f4eb0105ec635b7ce9d
Plamena Petkova <pl.petkova@gmail.com>
2025-07-24 20:08:45 +0300

    Added gmail icon

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -31,6 +31,16 @@ const Footer = () => {
                             d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path>
                     </svg>
                 </a>
+                <a className='btn btn-secondary' href='https://www.gmail.com'>
+                    <svg xmlns="http://www.w3.org/2000/svg"
+                        height="24"
+                        viewBox="0 0 24 24"
+                        width="24"
+                        className="fill-current">
+                        <path d="M20 4H4c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6
+           c0-1.1-.9-2-2-2zm0 4l-8 5-8-5V6l8 5 8-5v2z"/>
+                    </svg>
+                </a>
             </nav>
         </footer>
     );
